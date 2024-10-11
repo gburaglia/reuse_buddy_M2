0c7f3434af693489aaa8c15aebd5868e0cc89fbc
gburaglia <gabiburaglia@gmail.com>
2024-10-10 21:31:50 -0400

    milestone1 progress

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
+import os
 
 load_dotenv()
 
@@ -7,11 +9,20 @@ app =Flask(__name__, template_folder = 'templates', static_folder='static',stati
 
 @app.route('/')
 def index():
-    return render_template('about.html')
+    return render_template('about.html', active='index')
 
-@app.route('/milestone1')
+@app.route('/milestone1', methods=['GET','POST'])
 def interaction_1():
-    return render_template('milestone1.html', active='interaction_1')
+    if request.method =='POST':
+        f = request.files["imgFile"]
+        file_name = secure_filename(f.filename)
+        cwd = os.getcwd()
+        upld_path = cwd+'/static/imgs/' + file_name
+        f.save(upld_path)
+        img_path = 'imgs/'+file_name
+        return render_template('milestone1.html', active='interaction_1',imgPath = img_path)
+    else:
+        return render_template('milestone1.html', active='interaction_1')
 
 
 if __name__ == '__main__':
